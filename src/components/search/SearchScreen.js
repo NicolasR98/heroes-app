ffd323ccf307702d1f2e203d0117c6b2a487758b
Nicolas Rios <nicolas.rios@icareweb.com>
2022-01-06 12:57:59 +0100

    Add `useForm` hook

--- src/components/search/SearchScreen.js
+++ src/components/search/SearchScreen.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import { useForm } from '../../hooks/useForm';
 
 export const SearchScreen = () => {
+    const [{searchText}, handleInputChange] = useForm({
+        searchText: ''
+    });
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        console.log(searchText);
+    };
+
     return (
-        <div>
-            <h1>SearchScreen</h1>
-        </div>
+        <>
+            <h1>Search</h1>
+            <hr/>
+
+            <div className='row'>
+                <div className='col-6'>
+                    <form onSubmit={handleSearch}>
+                        <input 
+                            type='text'
+                            placeholder='Search a hero'
+                            className='form-control'
+                            name='searchText'
+                            autoComplete='off'
+                            onChange={handleInputChange}
+                            value={searchText}
+                        />
+
+                        <button
+                            className='btn btn-outline-primary mt-2'
+                        >
+                            Search...
+                        </button>
+                    </form>
+                </div>
+                
+            </div>
+        </>
     );
 };
