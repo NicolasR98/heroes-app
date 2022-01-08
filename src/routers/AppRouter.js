719309b0f4ba3617138807141b260fd01d899435
Nicolas Rios <nicolas.rios@icareweb.com>
2022-01-08 12:36:34 +0100

    Add

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { LoginScreen } from '../components/login/LoginScreen'
-import { PrivateRoute } from './PrivateRoute'
-import { DashboardRoutes } from './DashboardRoutes'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
+
+import { LoginScreen } from '../components/login/LoginScreen';
+import { DashboardRoutes } from './DashboardRoutes';
 
 export const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/login' element={<LoginScreen />} />
-                <Route 
-                    path='*' 
+                <Route
+                    path='/login'
+                    element={
+                        <PublicRoute>
+                            <LoginScreen />
+                        </PublicRoute>
+                    }
+                />
+                <Route
+                    path='*'
                     element={
                         <PrivateRoute>
                             <DashboardRoutes />
                         </PrivateRoute>
-                    } 
+                    }
                 />
             </Routes>
         </BrowserRouter>
-    )
-}
+    );
+};
