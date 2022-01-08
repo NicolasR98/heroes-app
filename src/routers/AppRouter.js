94637a2a0cda9f97fdeec7f1c6f6507062b68fdf
Nicolas Rios <nicolas.rios@icareweb.com>
2022-01-08 12:20:59 +0100

    Add `PrivateRoute`

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LoginScreen } from '../components/login/LoginScreen'
+import { PrivateRoute } from './PrivateRoute'
 import { DashboardRoutes } from './DashboardRoutes'
 
 export const AppRouter = () => {
@@ -8,7 +9,14 @@ export const AppRouter = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/login' element={<LoginScreen />} />
-                <Route path='*' element={<DashboardRoutes />} />
+                <Route 
+                    path='*' 
+                    element={
+                        <PrivateRoute>
+                            <DashboardRoutes />
+                        </PrivateRoute>
+                    } 
+                />
             </Routes>
         </BrowserRouter>
     )
